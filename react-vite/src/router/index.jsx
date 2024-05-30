fa04b742742599236d5e05b2d637683937819390
Charles Oller <charlesrello@gmail.com>
2024-05-30 18:46:29 -0400

    Scrollbar

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
-import Layout from './Layout';
+// import Layout from './Layout';
 import ServerViewLayout from '../components/ServerViewLayout/ServerViewLayout';
 import ServerView from '../components/ServerView/ServerView';
 
