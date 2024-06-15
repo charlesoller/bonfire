07dd9ced4c5502ae8d948be718619cebec57b3e1
Charles Oller <charlesrello@gmail.com>
2024-06-15 10:39:42 -0400

    added dep and updated seeder

--- app/seeds/seeder.py
+++ app/seeds/seeder.py
@@ -6,6 +6,7 @@ from app.models import (
 )
 from sqlalchemy.sql import text
 from datetime import datetime
+from flask.cli import AppGroup
 
 # Adds multiple users, servers, channels, and related data
 def seed_data():
@@ -17,6 +18,9 @@ def seed_data():
         User(username='user5', email='user5@example.com', hashed_password='hashed_password_5', role='user', created_at=datetime.now(), updated_at=datetime.now())
     ]
 
+    db.session.add_all(users)
+    db.session.commit()
+
     profile_images = [
         ProfileImage(url='https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_3x4.jpg', user_id=1),
         ProfileImage(url='https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg', user_id=2),
@@ -24,6 +28,9 @@ def seed_data():
         ProfileImage(url='https://i.pinimg.com/originals/c5/c1/be/c5c1bec89341caeed8b17fa75aad9b83.jpg', user_id=4),
         ProfileImage(url='https://i.pinimg.com/736x/eb/ae/0e/ebae0e4403a3c8d5ed8dcce05000f744.jpg', user_id=5)
     ]
+
+    db.session.add_all(profile_images)
+    db.session.commit()
 
     servers = [
         Server(name='Server1', description='First server', owner_id=1, created_at=datetime.now(), updated_at=datetime.now()),
@@ -33,6 +40,9 @@ def seed_data():
         Server(name='Server5', description='Fifth server', owner_id=5, created_at=datetime.now(), updated_at=datetime.now())
     ]
 
+    db.session.add_all(servers)
+    db.session.commit()
+
     server_images = [
         ServerImage(url='http://example.com/server1.jpg', server_id=1),
         ServerImage(url='http://example.com/server2.jpg', server_id=2),
@@ -41,6 +51,9 @@ def seed_data():
         ServerImage(url='http://example.com/server5.jpg', server_id=5)
     ]
 
+    db.session.add_all(server_images)
+    db.session.commit()
+
     server_users = [
         ServerUser(user_id=1, server_id=1, created_at=datetime.now(), updated_at=datetime.now()),
         ServerUser(user_id=2, server_id=2, created_at=datetime.now(), updated_at=datetime.now()),
@@ -48,7 +61,10 @@ def seed_data():
         ServerUser(user_id=4, server_id=4, created_at=datetime.now(), updated_at=datetime.now()),
         ServerUser(user_id=5, server_id=5, created_at=datetime.now(), updated_at=datetime.now())
     ]
-    
+
+    db.session.add_all(server_users)
+    db.session.commit()
+
     chat_rooms = [
         ChatRoom(name="super awesome chat"),
         ChatRoom(name="super average chat"),
@@ -56,6 +72,9 @@ def seed_data():
         ChatRoom(name="super fun chat"),
         ChatRoom(name="super boring chat")
     ]
+
+    db.session.add_all(chat_rooms)
+    db.session.commit()
 
     channels = [
         Channel(name='Channel1', server_id=1, owner_id=1),
@@ -65,6 +84,9 @@ def seed_data():
         Channel(name='Channel5', server_id=5, owner_id=5)
     ]
 
+    db.session.add_all(channels)
+    db.session.commit()
+
     channel_messages = [
         ChannelMessage(user_id=1, channel_id=1, text_field='Hello, world!'),
         ChannelMessage(user_id=2, channel_id=2, text_field='Hi there!'),
@@ -73,6 +95,9 @@ def seed_data():
         ChannelMessage(user_id=5, channel_id=5, text_field='Good night!')
     ]
 
+    db.session.add_all(channel_messages)
+    db.session.commit()
+
     chat_room_messages = [
         ChatRoomMessage(user_id=1, chat_room_id=1, text_field='Hello, universe!'),
         ChatRoomMessage(user_id=2, chat_room_id=2, text_field='Hi there!'),
@@ -80,6 +105,9 @@ def seed_data():
         ChatRoomMessage(user_id=4, chat_room_id=4, text_field='How\'s it going!'),
         ChatRoomMessage(user_id=5, chat_room_id=5, text_field='Hey!')
     ]
+
+    db.session.add_all(chat_room_messages)
+    db.session.commit()
 
     message_images = [
         MessageImage(url='http://example.com/message1.jpg', resource_type='channel', channel_message_id=1),
@@ -94,18 +122,24 @@ def seed_data():
         MessageImage(url='http://example.com/message10.jpg', resource_type='chat_room', chat_room_message_id=5)
     ]
 
+    db.session.add_all(message_images)
+    db.session.commit()
+
     reactions = [
-        Reaction(channel_message_id=1, resource_type='channel', emoji='😀', count=5),
-        Reaction(channel_message_id=2, resource_type='channel', emoji='😁', count=3),
-        Reaction(channel_message_id=3, resource_type='channel', emoji='😂', count=4),
-        Reaction(channel_message_id=4, resource_type='channel', emoji='🤣', count=2),
-        Reaction(channel_message_id=5, resource_type='channel', emoji='😃', count=6),
-        Reaction(chat_room_message_id=1, resource_type='chat_room', emoji='😄', count=5),
-        Reaction(chat_room_message_id=2, resource_type='chat_room', emoji='😅', count=3),
-        Reaction(chat_room_message_id=3, resource_type='chat_room', emoji='😆', count=4),
-        Reaction(chat_room_message_id=4, resource_type='chat_room', emoji='😉', count=2),
-        Reaction(chat_room_message_id=5, resource_type='chat_room', emoji='😊', count=6)
+        Reaction(channel_message_id=1, resource_type='channel', emoji='😀', count=1),
+        Reaction(channel_message_id=2, resource_type='channel', emoji='😁', count=1),
+        Reaction(channel_message_id=3, resource_type='channel', emoji='😂', count=1),
+        Reaction(channel_message_id=4, resource_type='channel', emoji='🤣', count=1),
+        Reaction(channel_message_id=5, resource_type='channel', emoji='😃', count=1),
+        Reaction(chat_room_message_id=1, resource_type='chat_room', emoji='😄', count=1),
+        Reaction(chat_room_message_id=2, resource_type='chat_room', emoji='😅', count=1),
+        Reaction(chat_room_message_id=3, resource_type='chat_room', emoji='😆', count=1),
+        Reaction(chat_room_message_id=4, resource_type='chat_room', emoji='😉', count=1),
+        Reaction(chat_room_message_id=5, resource_type='chat_room', emoji='😊', count=1)
     ]
+
+    db.session.add_all(reactions)
+    db.session.commit()
 
     user_reactions = [
         UserReaction(user_id=1, reaction_id=1),
@@ -115,6 +149,9 @@ def seed_data():
         UserReaction(user_id=5, reaction_id=5)
     ]
 
+    db.session.add_all(user_reactions)
+    db.session.commit()
+
     chat_room_users = [
         ChatRoomUser(user_id=1, chat_room_id=1),
         ChatRoomUser(user_id=2, chat_room_id=2),
@@ -123,10 +160,7 @@ def seed_data():
         ChatRoomUser(user_id=5, chat_room_id=5)
     ]
 
-    db.session.add_all(users + profile_images + servers + server_images + server_users + chat_rooms +
-                       channels + channel_messages + chat_room_messages + message_images + 
-                       reactions + user_reactions + chat_room_users)
-
+    db.session.add_all(chat_room_users)
     db.session.commit()
 
 # Uses a raw SQL query to TRUNCATE or DELETE the users table. SQLAlchemy doesn't
@@ -135,7 +169,7 @@ def seed_data():
 # incrementing primary key, CASCADE deletes any dependent entities. With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
-def undo_users():
+def undo_seeds():
     if environment == "production":
         db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
     else:
